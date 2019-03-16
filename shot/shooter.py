825bbfb365b1d6fd8c443b3976ad2308d2387a7e
kiddick <kiddick@users.noreply.github.com>
2019-03-16 23:14:49 +0300

    Add logs

--- shot/shooter.py
+++ shot/shooter.py
@@ -1,4 +1,3 @@
-import concurrent
 import datetime
 import logging
 from pathlib import Path
@@ -90,12 +89,14 @@ def make_weekly_movie(cam: Cam, executor):
                     sequence.append(str(img))
     sequence = check_sequence_for_gray_images(sequence)
     txt_clip = make_txt_movie(sequence, 100, executor)
+    logger.info(f'Composing clip for weekly movie ww{start.week_of_year}')
     image_clip = ImageSequenceClip(sequence, fps=100)
     clip = CompositeVideoClip([image_clip, txt_clip.set_position(('right', 'top'))], use_bgclip=True)
     movie_path = root / 'regular' / 'weekly' / f'ww{start.week_of_year}.mp4'
     movie_path.parent.mkdir(parents=True, exist_ok=True)
     movie_path = str(movie_path)
     clip.write_videofile(movie_path, audio=False)
+    logger.info(f'Finished with clip for weekly movie ww{start.week_of_year}')
     return movie_path
 
 
