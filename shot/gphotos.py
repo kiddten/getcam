16573c5fce0ec0260c87edb46272b782d2e86119
kiddick <kiddick@users.noreply.github.com>
2019-03-28 16:32:51 +0300

    Add throttling + debug info

--- shot/gphotos.py
+++ shot/gphotos.py
@@ -1,8 +1,10 @@
 import asyncio
+import time
 from pathlib import Path
 
 import aiohttp
 from aiogoogle import Aiogoogle
+from asyncio_throttle import Throttler
 from loguru import logger
 
 from shot import conf
@@ -86,36 +88,63 @@ class GooglePhotosManager:
             token = await self.raw_upload('/home/your/path')
             r = await self.upload_item(photos, album_id, token)
 
-    async def _upload_raw_task(self, session, headers, path: Path):
+    async def _upload_raw_task(self, session, throttler, headers, path: Path):
         headers['X-Goog-Upload-File-Name'] = str(path.name)
         logger.info(f'Uploading file {path}')
         with open(path, 'rb') as item:
             data = item.read()
-        result = await session.post(self.upload_url, headers=headers, data=data)
-        response = await result.text()
-        logger.info(f'Raw upload status {result.status} response for {path} : {response}')
+        async with throttler:
+            result = await session.post(self.upload_url, headers=headers, data=data)
+            response = await result.text()
+            logger.info(f'Raw upload status {result.status} response for {path} : {response}')
         if result.status != 200:
             logger.warning(f'Error during uploading {path} {response}')
-            return
-        return str(response)
+            return 'ERROR', str(path)
+        return str(response), str(path)
 
     async def batch_raw_upload(self, path: Path):
         headers = {
-            'Authorization': 'Bearer ' + self.user_cred.access_token,
+            'Authorization': 'Bearer ' + self.client.user_creds.access_token,
             'Content-Type': 'application/octet-stream',
             'X-Goog-Upload-Protocol': 'raw',
         }
-        tasks = []
+        # tasks = []
+        # connector = aiohttp.TCPConnector(limit=20)
+        start_time = time.time()
+        throttler = Throttler(rate_limit=299, period=60, retry_interval=.1)
+        # async with aiohttp.ClientSession(connector=connector, headers=headers) as session:
+        #     for item in sorted(path.iterdir()):
+        #         print(item)
+        #         tasks.append(self._upload_raw_task(session, throttler, headers, item))
+        #     tokens = await asyncio.gather(*tasks, return_exceptions=True)
+        tokens_tuples = await self.upload_items(throttler, headers, sorted(path.iterdir()))
+        tokens = []
+        broken_files = 0
+        for item in tokens_tuples:
+            if item[0] == 'ERROR':
+                logger.critical(f'ERROR with uploading {item[1]}')
+                broken_files += 1
+            else:
+                tokens.append(item[0])
+        logger.critical(f'BROKEN files count {broken_files}')
+        # tokens = [i[0] for i in tokens_tuples if i]
+        # logger.info(f'Tokens for {path}: {tokens}')
+        empty_count = len([x for x in tokens if x is None])
+        logger.critical(f'TOTAL TIMEEE::: {time.time() - start_time}')
+        logger.critical(f'TOTAL LENGTH:: {len(tokens)} EMPTY TOKENS {empty_count}')
+        return tokens_tuples, tokens
+
+    async def upload_items(self, throttler, headers, items):
         connector = aiohttp.TCPConnector(limit=20)
+        tasks = []
         async with aiohttp.ClientSession(connector=connector, headers=headers) as session:
-            for item in sorted(path.iterdir()):
-                print(item)
-                tasks.append(self._upload_raw_task(session, headers, item))
+            for item in items:
+                tasks.append(self._upload_raw_task(session, throttler, headers, item))
             tokens = await asyncio.gather(*tasks, return_exceptions=True)
-        logger.info(f'Tokens for {path}: {tokens}')
         return tokens
 
     async def batch_upload_item(self, photos, album, tokens):
+        results = []
         empty_counter = 0
         for chunk in chunks(tokens, 50):
             new_media_items = []
@@ -129,22 +158,35 @@ class GooglePhotosManager:
                 'newMediaItems': new_media_items,
                 'albumId': album
             }
-            await self.client.as_user(photos.mediaItems.batchCreate(json=data))
+            response = await self.client.as_user(photos.mediaItems.batchCreate(json=data))
             logger.info(f'Images count: {len(chunk)} successfully added to album {album}')
+            results.extend(response['newMediaItemResults'])
         if empty_counter:
             logger.critical(f'Detected {empty_counter} empty tokens!')
-        return logger.info(f'All images: {len(tokens)} successfully added to album {album}')
+        for item in results:
+            logger.success(item['uploadToken'])
+            logger.success(item['status'])
+            # ma = item.get('mediaItem'):
+            # logger.success(item.get('mediaItem').get('filename'))
+        logger.info(f'All images: {len(tokens)} successfully added to album {album}')
+        return [(item['uploadToken'], item['status']) for item in results]
 
     async def batch_upload(self, directory: Path):
         async with self.client as client:
-            # await self.refresh_token()
+            creds = await self.refresh_token(client)
+        logger.critical(creds)
+        self.client.user_creds = creds
+        self.client.active_session = None
+        async with self.client as client:
             photos = await client.discover('photoslibrary', 'v1')
             album_name = get_album_name(directory)
+            # album_name = 'TEST+' + get_album_name(directory)
             album_id = await self.create_or_retrieve_album(photos, album_name)
-            tokens = await self.batch_raw_upload(directory)
-            r = await self.batch_upload_item(photos, album_id, tokens)
+            tokens_tuples, tokens = await self.batch_raw_upload(directory)
+            result = await self.batch_upload_item(photos, album_id, tokens)
 
-    async def refresh_token(self):
+    async def refresh_token(self, client):
         # async with self.client as client:
-        response = await self.client.oauth2.refresh(self.user_cred.as_dict(), self.client_cred.as_dict())
+        response = await client.oauth2.refresh(self.user_cred.as_dict(), self.client_cred.as_dict())
         logger.info(f'access_token refreshed: {response}')
+        return response
