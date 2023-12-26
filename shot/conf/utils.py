da5cc3aa6abe867462f950edddfe30332a066187
kiddten <rustam@rustams-MacBook-Pro.local>
2023-12-26 23:01:00 +0300

    .gitignore

--- shot/conf/utils.py
+++ shot/conf/utils.py
@@ -4,7 +4,7 @@ import os
 def root_directory():
     root_path = os.path.dirname(__file__)
     # TODO fix lookup
-    while root_path and 'settings.template.yaml' not in os.listdir(root_path):
+    while root_path and 'settings.template.json' not in os.listdir(root_path):
         root_path = os.path.dirname(root_path)
     return root_path
 
